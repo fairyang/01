39b75b8a07cccdfa8127a8d4ff6d5be91bc1e438
unknown <3345660949@qq.com>
2019-11-18 18:17:12 +0800

    second commit

--- 正弦波实例.py
+++ 正弦波实例.py
@@ -6,7 +6,7 @@ plt.plot(a,np.cos(2*np.pi*a),'r--')
 plt.xlabel('横轴:时间',fontproperties='Simhei',fontsize=15,color='green')
 plt.ylabel('纵轴:振幅',fontproperties='simhei',fontsize=15,color='blue')
 plt.title(r'正弦波实例 $y=cos(2\pi x)$',fontproperties='Simhei',fontsize=25)
-plt.text(2,1,r'$\mu=1005',fontsize=15)
+plt.text(2,1,r'$\mu=1005$',fontsize=15)
 
 plt.axis([-1,6,-2,2])
 plt.grid(True)#添加网格
